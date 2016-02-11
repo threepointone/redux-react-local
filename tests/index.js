7d5f515387d389f78206cc401a51e446c8033d99
Sunil Pai <threepointone@gmail.com>
2016-02-11 11:08:20 +0530

    test stub

--- tests/index.js
+++ tests/index.js
@@ -4,10 +4,43 @@ import React, {Component, PropTypes} from 'react';
 import {render, unmountComponentAtNode} from 'react-dom';
 import {findRenderedDOMComponentWithTag, Simulate} from 'react-addons-test-utils';
 
+import {Root, local} from '../src';
+
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 expect.extend(expectJSX);
 
 describe('local', () => {
-  it('stubby stub', () => {});
+  let node;
+  beforeEach(() => node = document.createElement('div'));
+  afterEach(() => unmountComponentAtNode(node));
+
+  it('stubby stub', done => {
+    @local({
+      ident: 'app',
+      initial: 0,
+      reducer(state, {meta, me}){
+        if (me){
+          switch (meta.type){
+            case 'increment': return state + 1;
+          }
+        }
+        return state;
+      }
+    })
+    class App extends Component{
+      componentDidMount(){
+        expect(typeof this.props.dispatch).toEqual('function');
+        expect(typeof this.props.$).toEqual('function');
+        expect(this.props.state).toEqual(0);
+        done();
+      }
+      render(){
+        return null;
+      }
+    }
+
+    render(<Root><App/></Root>, node);
+
+  });
 });
