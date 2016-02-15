6c21984971f6909796ab539a1bb6d4df44952853
Sunil Pai <threepointone@gmail.com>
2016-02-16 02:16:54 +0530

    lint root

--- example/root.js
+++ example/root.js
@@ -1,28 +1,28 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react'
 
 // redux
-import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
 
 // redux-saga
-import createSagaMiddleware from 'redux-saga';
-import {Sagas} from 'react-redux-saga';
+import createSagaMiddleware from 'redux-saga'
+import { Sagas } from 'react-redux-saga'
 
 // optimist
-import optimist from 'redux-optimist';
-import {Optimist} from 'react-redux-optimist';
+import optimist from 'redux-optimist'
+import { Optimist } from 'react-redux-optimist'
 
 // redux-react-local
-import {reducer} from '../src';
+import { reducer } from '../src'
 
 // fsa
-import ensureFSA from './ensure-fsa';
+import ensureFSA from './ensure-fsa'
 
 // perf
-import {batchedSubscribe} from 'redux-batched-subscribe';
-import {unstable_batchedUpdates as batchedUpdates} from 'react-dom';
+import { batchedSubscribe } from 'redux-batched-subscribe'
+import { unstable_batchedUpdates as batchedUpdates } from 'react-dom'
 
-function makeStore(reducers = {}, initial = {}, middleware = []){
+function makeStore(reducers = {}, initial = {}, middleware = []) {
   // create a redux store
   return createStore(
     // reducer
@@ -34,42 +34,42 @@ function makeStore(reducers = {}, initial = {}, middleware = []){
     initial || {},
     // middleware
     compose(applyMiddleware(...middleware), batchedSubscribe(batchedUpdates))
-  );
+  )
 }
 
 
-export default class Root extends Component{
+export default class Root extends Component {
   // optionally accept middleware/reducers to add on to the redux store
   static propTypes = {
     middleware: PropTypes.array,
     reducers: PropTypes.object
   };
 
-  *middle(){
-    if (this.props.middleware){
-      yield* this.props.middleware;
+  *middle() {
+    if (this.props.middleware) {
+      yield* this.props.middleware
     }
-    yield this.sagaMiddleware;
-    if (process.env.NODE_ENV === 'development'){
-      yield ensureFSA;
+    yield this.sagaMiddleware
+    if (process.env.NODE_ENV === 'development') {
+      yield ensureFSA
     }
   }
 
-  sagaMiddleware = createSagaMiddleware();
+  sagaMiddleware = createSagaMiddleware()
 
   store = makeStore(
     this.props.reducers,
     this.props.initial,
     this.middle()
-  );
+  )
 
-  render(){
-    return <Provider store={this.store}>
+  render() {
+    return (<Provider store={this.store}>
       <Sagas middleware={this.sagaMiddleware}>
         <Optimist>
           {this.props.children}
         </Optimist>
       </Sagas>
-    </Provider>;
+    </Provider>)
   }
 }
