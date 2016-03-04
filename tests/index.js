815885055087c95b24b7bcf17c91a305ed7667ad
Sunil Pai <threepointone@gmail.com>
2016-03-04 15:13:46 +0530

    some tree tests

--- tests/index.js
+++ tests/index.js
@@ -390,9 +390,22 @@ describe('redux-react-local', () => {
   })
 
   describe('tree', () => {
-    it('makes immutable copies')
-    it('has log n writes')
-    it('has log n reads')
+    it('create', () => {
+      let t = T.make()
+      expect(T.toObject(t)).toEqual({})
+    })
+    it('set/get/del', () => {
+      let t = T.make()
+      expect(T.get(t, 'x')).toEqual(undefined)
+      let t2 = T.set(t, 'x', 123)
+      expect(T.get(t2, 'x')).toEqual(123)
+      let t3 = T.del(t2, 'x')
+      expect(T.toObject(t3)).toEqual({})
+    })
+
+    it('entries/toObject')
+
+    it('lots of keys')
   })
 
 
